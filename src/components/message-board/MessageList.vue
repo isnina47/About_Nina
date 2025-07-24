0a77218d7f0ddb3d006532a61c0687c75eb2fa26
Nina Yu <ninaaaaa1327@gmail.com>
2025-07-24 16:18:35 +0800

    feat : 新增 msgList  元件

---
+++ src/components/message-board/MessageList.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul class="space-y-3">
+    <li v-for="(msg, idx) in messages" :key="idx" class="p-4 border rounded">
+      <p class="font-semibold">{{ msg.name }}：</p>
+      <p class="text-gray-700">{{ msg.content }}</p>
+      <p class="text-sm text-gray-400">{{ formatDate(msg.createdAt) }}</p>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+  const props = defineProps({
+    messages: Array
+  })
+
+  const formatDate = iso => new Date(iso).toLocaleString()
+</script>
